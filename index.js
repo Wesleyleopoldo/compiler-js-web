443391e4f6db2bf54a79817ad1be873dab0991b3
desenvolvimentowesleysilva@gmail.com <desenvolvimentowesleysilva@gmail.com>
2025-09-13 16:21:16 -0300

    fix(SandBox, AutoScroll): Adiciona correção a sandBox e Insere autoscroll no terminal
    Signed-off-by: desenvolvimentowesleysilva@gmail.com <desenvolvimentowesleysilva@gmail.com>

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
 
 
-const iframe = document.getElementById("sandbox");  
+const iframe = document.getElementById("sandbox");
 
 iframe.srcdoc = `
   <!doctype.html>
-  <meta http-equiv="Content-Security-Policy" content="
-  default-src 'none';
-  script-src 'unsafe-inline' 'unsafe-eval';
-  connect-src 'none';
-  img-src 'none';
-  style-src 'unsafe-inline';
-
-  <body>
-  <script>
-    const send = (type, message) => parent.postMessage({type, message}, '*');
-    console.log = (...args) => send('log', args.join(' '));
-    console.error = (...args) => send('error', args.join(' '));
-    
-    window.addEventListener('message', (e) => {
-       if (!e.data || e.data.type !== 'run') return;
-       try {
-          new Function(e.data.code)();
-          send('done', 'Execução finalizada');
-       } catch(error) {
-          send('error', error.message);
-       }
-    });
-  </script>
-  </body>
+    <html>
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="
+        default-src 'none';
+        script-src 'unsafe-inline' 'unsafe-eval';
+        connect-src 'none';
+        img-src 'none';
+        style-src 'unsafe-inline';
+        ">
+      </head>
+      <body>
+        <script>
+          const send = (type, message) => parent.postMessage({type, message}, '*');
+          console.log = (...args) => send('log', args.join(' '));
+          console.error = (...args) => send('error', args.join(' '));
+          
+          window.addEventListener('message', (e) => {
+            if (!e.data || e.data.type !== 'run') return;
+            try {
+                new Function(e.data.code)();
+                send('done', 'Execução finalizada');
+            } catch(error) {
+                send('error', error.message);
+            }
+          });
+        </script>s
+      </body>
+    </html>
   "
   `
 
 const divOutput = document.getElementById("output");
 
 function printLine(type, message) {
-   let color = "white";
+  const divBash = document.getElementById("bash");
+  let color = "white";
 
-   if(type === "error") color = "red";
-   if(type === "done") color = "lightgreen";
+  if (type === "error") color = "red";
+  if (type === "done") color = "lightgreen";
 
-   divOutput.innerHTML += `<span style="color:${color}">>> ${message}</span><br>`;
+  divOutput.innerHTML += `<span style="color:${color}">>> ${message}</span><br>`;
+  
+  divBash.scrollTop = divBash.scrollHeight;
 }
 
 window.addEventListener("message", (e) => {
-   if(!e.data) return;
-   printLine(e.data.type, e.data.message);
+  if (!e.data) return;
+  printLine(e.data.type, e.data.message);
 });
 
 function runCode() {
-   divOutput.innerHTML = "";
-   const code = document.getElementById("code").value;
-   iframe.contentWindow.postMessage({type: "run", code}, "*");
+  const code = document.getElementById("code").value;
+  iframe.contentWindow.postMessage({ type: "run", code }, "*");
 }
